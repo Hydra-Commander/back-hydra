81e341f6f81a36088046ba4ee4de77ef41365164
Enderson <enderson.linhares20@gmail.com>
2025-01-30 20:58:31 -0300

    feat: initing project

---
+++ src/main/java/com/hc/hydracommander/HydracommanderApplication.java
@@ -0,0 +1,13 @@
+package com.hc.hydracommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydracommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydracommanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hc/hydracommander/HydracommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.hydracommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydracommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
