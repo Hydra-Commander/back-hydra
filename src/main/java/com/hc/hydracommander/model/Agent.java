78e610dc9e08d24f7767f0fbe6f3b387dc81d1f6
Erivando Castro <163551939+ErivandoCastro@users.noreply.github.com>
2025-01-30 22:24:30 -0300

    feat: creating agent and user in model

--- src/main/java/com/hc/hydracommander/model/Agent.java
+++ src/main/java/com/hc/hydracommander/model/Agent.java
@@ -1,4 +1,22 @@
 package com.hc.hydracommander.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity(name = "Agent")
+@Table(name = "agents")
+@Data
 public class Agent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String hostname;
+    private String ipAdress;
+    //private StatusAgent status;
+    private LocalDate lastActive;
+
 }
--- src/main/java/com/hc/hydracommander/model/User.java
+++ src/main/java/com/hc/hydracommander/model/User.java
@@ -1,4 +1,16 @@
 package com.hc.hydracommander.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity(name = "User")
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String username;
+    private String password;
 }
